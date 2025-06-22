ecd39176cf4053854b744826b3293c31b739fa05
brian.musili <brian.musili@systechafrica.com>
2025-06-22 14:15:47 +0300

    End to end todo list simple app

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-const TodoList = ({todos}) => {
-
-  // todos -> a list of 4 items 
-  // todos -> when you map -> it will iterate through each item -> and render the output <4 times> in line 12 we are mapping todos accepted by props
-  // todos -> ignore line 16 -> will understand later
-
-  // ! Map Method in JS
-  //? Given an array of list, the map method iterates through each item in the array
+const TodoList = ({ myTodos, handleDeleteTodo, handleUpdate }) => {
   return (
     <div>
-      {todos.map((todo) => (
-        // <p key={todo.id}> {todo.name}</p>
-        // <TodoItem key={todo.id} todo = {todo} />
-        <div key={todo.id} className="bg-gray-100  my-3 p-3 rounded-lg flex justify-between items-center">
+      {myTodos.map((todo) => (
+        <div
+          key={todo.id}
+          className="bg-gray-100  my-3 p-3 rounded-lg flex justify-between items-center"
+        >
           <div>
             <h1>
-              <span className="font-semibold mr-2"> Name:</span> {todo.name}
+              <span className="font-semibold mr-2"> Name:</span> {todo.todoName}
             </h1>
             <h1>
               <span className="font-semibold mr-2">Desc: </span> {todo.desc}
             </h1>
+            <div>{todo.completed && "✅"}</div>
           </div>
-          <div>{todo.isComplete && "✅"}</div>
+          <div className="flex gap-2 items-center">
+            <button className="p-2 bg-blue-600 rounded-md text-white" onClick={() => handleUpdate(todo.id)}>
+              Update
+            </button>
+            <button
+              className="p-2 bg-red-600 rounded-md text-white hover:cursor-pointer"
+              onClick={() => handleDeleteTodo(todo.id)}
+            >
+              Delete
+            </button>
+          </div>
         </div>
-       
       ))}
     </div>
   );
--- src/views/TodoListApp.jsx
+++ src/views/TodoListApp.jsx
@@ -1,52 +1,173 @@
+import { useEffect, useState } from "react";
 import TodoList from "../components/TodoList";
+import TodoForm from "../components/TodoForm";
 
 
+const url = "https://jsonplaceholder.typicode.com/todos";
 const TodoListApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState("")
+
+  const [todoData, setTodoData] = useState({
+    id: null,
+    todoName: "",
+    desc: "",
+    completed: false,
+  });
+
+  const handleInputChange = (event) => {
+    setTodoData({
+      ...todoData,
+      [event.target.id]:
+        event.target.type === "checkbox"
+          ? event.target.checked
+          : event.target.value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (isEditing) {
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) =>
+          todo.id === todoData.id
+            ? {
+                ...todo,
+                todoName: todoData.todoName,
+                desc: todoData.desc,
+                completed: todoData.completed,
+              }
+            : todo
+        )
+      );
+      setIsEditing(false);
+    } else {
+      setTodos((prevTodos) => [
+        ...prevTodos,
+        {
+          ...todoData,
+          id: Math.floor(Math.random() * 1000),
+        },
+      ]);
+
+      // try {
+      //   const response = await fetch(url, {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //     body: JSON.stringify(
+      //       {
+      //             ...todoData,
+      //             id: Math.floor(Math.random() * 1000),
+      //           },
+      //     ),
+      //   });
+  
+      //   if (response.ok) {
+      //     toast.success("Message sent successfully! 🎉");
+      //     reset();
+      //   } else {
+      //     const errorDetails = await response.json();
+      //     toast.error("Failed to send message. Please try again.");
+      //     console.error("Error sending email:", errorDetails.message);
+      //   }
+    }
+
+    setTodoData({
+      todoName: "",
+      desc: "",
+      completed: false,
+    });
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const handleUpdate = (id) => {
+    setIsEditing(true);
+    const todoToUpdate = todos.find((todo) => todo.id === id);
+
+    setTodoData({
+      id: todoToUpdate.id,
+      todoName: todoToUpdate.todoName,
+      desc: todoToUpdate.desc,
+      completed: todoToUpdate.completed,
+    });
+  };
 
 
-    const todos = [
-      {
-        id: 3,
-        name: "Read Book",
-        desc: "Finish chapter 5 of React guide",
-        isComplete: true,
-      },
-      {
-        id: 5,
-        name: "Call Mom",
-        desc: "Catch up over the weekend",
-        isComplete: true,
-      },
-      {
-        id: 1,
-        name: "Doing Shopping",
-        desc: "At QuickMart",
-        isComplete: false,
-      },
+  useEffect(() =>{
+    async function getData() {
 
-      {
-        id: 4,
-        name: "Write Blog Post",
-        desc: "Topic: React hooks explained",
-        isComplete: false,
-      },
-    ];
-      
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        console.log(response);
+        
+        if (!response.ok) {
+          setError("Failed To Fetch The data")
+          throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+       
+        
+      setTodos(json.slice(0, 10).map(todo => ({
+          id: todo.id,
+          todoName: todo.title,
+          desc: "No description available",
+          completed: todo.completed,
+        })));
+        
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error.message);
+      }finally{
+        setIsLoading(false);
+      }
+    }
+    
+    getData();
+  },[])
+  
+
+
   return (
     <div>
-        <h1 className="text-2xl text-gray-700 text-center font-bold">Todo List App</h1>
+      <h1 className="text-2xl text-gray-700 text-center font-bold">
+        Todo List App
+      </h1>
 
-        <div className="my-5">
-            {/* Add form to enter the todo list ->  form inputs -> name, desc, checkbox(isComplete) */}
-            {/* Add todo form component  */}
-        </div>
+      <div className="my-5">
+        <TodoForm
+          todoData={todoData}
+          isEditing={isEditing}
+          handleInputChange={handleInputChange}
+          handleFormSubmit={handleFormSubmit}
+        />
+      </div>
 
+      {isLoading ? (
+        <p className="text-center text-gray-500">Loading...</p>
+      ) : (
         <div>
-            {/* div rendering our todo list items */}
-            <TodoList todos ={todos}/>
+          {/* div rendering our todo list items */}
+          <TodoList
+            myTodos={todos}
+            handleDeleteTodo={handleDeleteTodo}
+            handleUpdate={handleUpdate}
+          />
         </div>
-    </div>
-  )
-}
+      )}
 
-export default TodoListApp
+      {error && <p className="text-center text-red-500">{error}</p>}
+    </div>
+  );
+};
+
+export default TodoListApp;
